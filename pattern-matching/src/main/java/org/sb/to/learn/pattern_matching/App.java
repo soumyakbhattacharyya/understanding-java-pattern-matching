8b8e44313577763240b260b56576699c414d1c45
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2018-02-20 19:24:41 +0530

    initial commit

---
+++ pattern-matching/src/main/java/org/sb/to/learn/pattern_matching/App.java
@@ -0,0 +1,13 @@
+package org.sb.to.learn.pattern_matching;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
